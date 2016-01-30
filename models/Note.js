9229234ead5286a7ecf8821a19db9d58673c0fd9
Aaron Hartwig <aaronhartwig7@gmail.com>
2016-01-30 15:21:51 -0600

    remove unneccesary models and apis

--- models/Note.js
+++
@@ -1,9 +0,0 @@
-var mongoose     = require('mongoose');
-var Schema       = mongoose.Schema;
-
-var Note = new Schema({
-    title: String,
-    body: String
-});
-
-module.exports = mongoose.model('Note', Note);
